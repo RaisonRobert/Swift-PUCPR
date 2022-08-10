57130ad48e4eb131bd4b7012175c96128443a2c3
Raison Robert Monteiro de Sá <raison.robert@pucpr.edu.br>
2022-08-10 10:34:09 -0400

    Contents

--- enums_optionals.playground/Contents.swift
+++ enums_optionals.playground/Contents.swift
@@ -1,1 +1,32 @@
+// Enum
+
+import Darwin
+enum AppleDevice{
+    case iPhone
+    case iPad
+    case AppleWatch
+    case AppleTV
+}
+print(AppleDevice.iPhone)
+
+var dispositivo = AppleDevice.AppleTV
+var dispositivo2: AppleDevice = . iPad
+
+enum AppleDevice2{
+    case iPhone(String)
+    case iPad(String)
+    case AppleWatch(String)
+    case AppleTV
+}
+
+var dispositivo3: AppleDevice2 = .iPhone("12")
+
+switch(dispositivo2){
+case .iPhone: fallthrough
+case .iPad: print("IOS")
+case .AppleTV: print("TVIOS")
+case.AppleWatch: print("WatchOS")
+    
+//default:print(" Dispositivo Inválido")
+}
 
